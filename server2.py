97048a1a6beeefd4c1f8e62b141e476bbf77f646
jtmk4752 <jtmk4752@yahoo.co.jp>
2021-11-16 15:32:01 +0900

    test

--- server2.py
+++ server2.py
@@ -1,11 +1,14 @@
 import socket
 import threading
 from datetime import datetime
+import RPi.GPIO as GPIO
+import time
 
 HOST_IP = "192.168.200.2" # サーバーのIPアドレス
 PORT = 9979 # 使用するポート
 CLIENTNUM = 3 # クライアントの接続上限数
 DATESIZE = 1024 # 受信データバイト数
+pin = 27
 
 class SocketServer():
     def __init__(self, host, port):
@@ -39,9 +42,42 @@ class SocketServer():
             while True:
                 # クライアントからデータ受信
                 rcv_data = client_socket.recv(DATESIZE)
-                if rcv_data:
-                    client_socket.send(rcv_data)
-                    print('[{0}] recv date : {1}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), rcv_data.decode('utf-8')) )
+                if rcv_data :
+                    if rcv_data == "1000000":
+                        client_socket.send(rcv_data)
+
+                        GPIO.setmode(GPIO.BCM)
+                        GPIO.setup(pin,GPIO.OUT,initial=GPIO.LOW)
+
+                        p = GPIO.PWM(pin,1)
+                        for i in range(3):
+                                p.start(50)
+                                p.ChangeFrequency(622.254)
+                                time.sleep(0.8)
+                                p.stop()
+                                time.sleep(0.1)
+                        GPIO.cleanup()
+
+                        print('[{0}] recv date : {1}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), rcv_data.decode('utf-8')) )
+                    else:
+                        client_socket.send(rcv_data)
+                        GPIO.setmode(GPIO.BCM)
+                        GPIO.setup(pin,GPIO.OUT,initial=GPIO.LOW)
+
+                        p = GPIO.PWM(pin,1)
+                        p.start(50)
+                        for i in range(5):
+                                p.start(50)
+                                p.ChangeFrequency(493.9)
+                                time.sleep(0.1)
+                                p.ChangeFrequency(196)
+                                time.sleep(0.6)
+                                p.stop()
+                                time.sleep(1)
+                        p.stop()
+                        GPIO.cleanup()
+                        
+                        print('[{0}] recv date : {1}'.format(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), rcv_data.decode('utf-8')) )                        
                 else:
                     break
 
@@ -50,3 +86,28 @@ class SocketServer():
 if __name__ == "__main__":
     
     SocketServer(HOST_IP, PORT).run_server()
+
+
+
+
+
+import RPi.GPIO as GPIO
+import time
+
+pin = 27
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(pin,GPIO.OUT,initial=GPIO.LOW)
+
+p = GPIO.PWM(pin,1)
+p.start(50)
+for i in range(5):
+        p.start(50)
+        p.ChangeFrequency(493.9)
+        time.sleep(0.1)
+        p.ChangeFrequency(196)
+        time.sleep(0.6)
+        p.stop()
+        time.sleep(1)
+p.stop()
+GPIO.cleanup()
